386f64d1cac83a3132473f33ceb57797c3017966
Vladimir Shytsko <shytsko.v@gmail.com>
2023-08-09 17:02:18 +0300

    Add admin models

--- seminars/homework3/admin.py
+++ seminars/homework3/admin.py
@@ -3,15 +3,31 @@ from .models import Customer, Product, Order
 
 
 class CustomerAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'email']
+    ordering = ['name']
+    list_filter = ['registration_date']
+
+    fields = ['name', 'email', 'tel', 'address', 'registration_date']
+    readonly_fields = ['registration_date']
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['name', 'price', 'count']
+    ordering = ['name']
+    list_filter = ['price', 'count']
+    search_fields = ['description']
+
+    fields = ['name', 'description', 'price', 'count', 'create_date']
+    readonly_fields = ['create_date']
 
 
 class OrderAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['pk', 'customer', 'total_amount', 'date']
+    ordering = ['customer']
+    list_filter = ['customer', 'products', 'total_amount', 'date']
+
+    fields = ['customer', 'date', 'total_amount']
+    readonly_fields = ['date', 'total_amount']
 
 
 admin.site.register(Customer, CustomerAdmin)
