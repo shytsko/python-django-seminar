3bd2d1ce78029b26b81fc9e6955ccd359c1d5a3b
Vladimir Shytsko <shytsko.v@gmail.com>
2023-08-09 16:46:35 +0300

    Add groups and users

--- seminars/homework3/admin.py
+++ seminars/homework3/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Customer, Product, Order
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
