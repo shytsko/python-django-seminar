beb867c72c5314dc65784233a091507717bd9daa
Vladimir Shytsko <shytsko.v@gmail.com>
2023-08-11 20:10:17 +0300

    Finish

--- seminars/seminars/settings.py
+++ seminars/seminars/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9jk5v7fvkrd^z#68upmh)ym-31*5upxk+i)riqorxk26%tw9=='
+# SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ['192.168.100.100',
-                 '127.0.0.1', ]
+                 '127.0.0.1',
+                 'shytskov.pythonanywhere.com', ]
 
 INTERNAL_IPS = [
     '127.0.0.1',
@@ -87,9 +93,20 @@ WSGI_APPLICATION = 'seminars.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shytskov$default',
+        'USER': 'shytskov',
+        # 'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'PASSWORD': 'mysql123',
+        'HOST': 'shytskov.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'", 'charset': 'utf8mb4',
+        },
     }
 }
 
@@ -126,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
